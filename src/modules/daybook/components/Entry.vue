7f7928286c7736d71187c860ecd620e6fc20c189
Patricio Hernan Muriega <pmuriega@TELECENTRO.local>
2021-11-30 11:47:22 -0300

    Primer commit

---
+++ src/modules/daybook/components/Entry.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="entry-container mb-3 pointer p-2"
+  @click="$router.push({name: 'entry', params: { id: 10}})">
+      <div class="entry-title d-flex">
+          <span class="text-success fs-5 fw-bold">15</span>
+          <span class="mx-1 fs-5">julio</span>
+          <span class="mx-2 font-weigth-light">2021</span>
+      </div>
+
+      <div class="entry-description">
+          Exercitation do mollit irure dolore cupidatat. Aliqua do enim ea et irure nulla labore labore consequat. Enim enim elit veniam commodo elit est mollit anim sunt. Occaecat pariatur est in veniam ex ipsum in labore ipsum excepteur. Aliquip enim est Lorem et culpa ad exercitation eiusmod magna irure ex esse. Minim aliqua ut cillum eiusmod amet tempor velit aute non mollit occaecat. Veniam nulla cillum id amet minim mollit aute reprehenderit nostrud esse commodo.
+      </div>
+  </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+.entry-container {
+    border-bottom: 1px solid #2c3e50;
+    transition: 0.2s all ease-in;
+
+    &:hover {
+        background-color: lighten($color: grey, $amount: 45);
+        transition: 0.2s all ease-in;
+    }
+    .entry-description {
+        font-size: 12px;
+    }
+}
+</style>
---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,23 @@
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text: 'Cupidatat velit do ad in consequat sit id cupidatat ea voluptate. Occaecat anim consequat exercitation sunt eiusmod pariatur qui. Nulla aliqua dolore aliquip officia velit fugiat nisi aliqua veniam nisi pariatur quis. Velit eiusmod elit fugiat non cillum labore. Quis aliqua eu est anim ipsum esse occaecat deserunt est occaecat occaecat ex. Cillum irure labore id et aute sunt irure deserunt culpa exercitation culpa. Aliquip cillum ad fugiat consequat consectetur velit.',
+            picture: null
+        },
+        {
+            id: new Date().getTime() +1000,
+            date: new Date().toDateString(),
+            text: 'Nisi in id aliquip fugiat dolore laborum exercitation sint est aliquip velit deserunt aliqua est. Qui fugiat in qui aute aliquip ipsum dolore eu eu Lorem excepteur dolor. Veniam ad ipsum exercitation veniam laborum deserunt dolore pariatur sit ad.',
+            picture: null
+        },
+        {
+            id: new Date().getTime() +2000,
+            date: new Date().toDateString(),
+            text: 'Magna excepteur laboris anim Lorem esse non veniam pariatur aliqua est nulla. Est consequat aliquip excepteur cupidatat minim voluptate cillum sit non officia. Est magna non eiusmod consequat eiusmod quis. Deserunt culpa aute sint deserunt proident minim. Eiusmod dolore occaecat aliquip fugiat eiusmod magna deserunt. Ex do officia aliquip irure sint tempor. Reprehenderit dolore id dolore minim eu laborum velit pariatur qui quis esse est.',
+            picture: null
+        },
+    ]
+})
